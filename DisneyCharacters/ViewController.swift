cf0ff93b7edcc628787394d2714e566aeb8d9ab1
João Pedro Cappelletto D'Agnoluzzo <joao.dagnoluzzo@gmail.com>
2024-07-24 20:21:33 -0300

    Initial Commit

---
+++ DisneyCharacters/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DisneyCharacters
+//
+//  Created by João Pedro C. D'Agnoluzzo on 24/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view?.backgroundColor = UIColor.cyan
+        
+    }
+
+
+}
+
